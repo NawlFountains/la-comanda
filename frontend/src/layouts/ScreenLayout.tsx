8111fec7ea3b2aa0a4a33fb641667f5cd9663329
Nahuel <nahuelfuentes2001@gmail.com>
2026-06-25 07:04:02 -0300

    style: improve login screen ui

--- frontend/src/layouts/ScreenLayout.tsx
+++ frontend/src/layouts/ScreenLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ( { children } : { children : React.ReactNode }) {
 	return (
-	<div className='min-h-screen bg-neutral-200 flex flex-col items-center py-4'>
+	<div className='h-screen w-screen bg-neutral-200 flex flex-col items-center'>
 	{children}
 	</div>
 	)
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,51 +1,86 @@
 import {buttonVariants} from "../components/ButtonStyles"
 import ErrorMessage from "../components/ErrorMessage"
-import {useLogin} from "../hooks/useLogin"
+import { useLogin } from "../hooks/useLogin"
+import { useState } from "react"
 import ScreenLayout from "../layouts/ScreenLayout"
+import {EyeIcon, EyeSlashIcon} from "../components/Icons"
+import { Link } from "react-router-dom"
 
 export default function Login() {
 	const { email, setEmail,
 		password, setPassword,
 		errors, loading,
 		handleLogin } = useLogin()
+	const [showPassword, setShowPassword] = useState(false)
 
 	return (
 		<>
 		<ScreenLayout>
-			<div className="bg-neutral-100 flex flex-col text-center items-center w-2/3 rounded-xl gap-7 p-2">
-			 <h1 className="text-xl py-2">On login screen </h1>
-			 <div>
-			 <div className="flex flex-col sm:flex-row gap-2">
-			 	<p className="sm:w-[90px] my-auto text-left text-neutral-600">
-					Email
-				</p>
-				<input 
-					value={email}
-					onChange={e => setEmail(e.target.value)}
-					className="p-2 border border-neutral-200 rounded-xl"
-					placeholder="email"/>
+			<div className="bg-neutral-100 flex flex-col text-center items-center w-full max-w-xl rounded-xl gap-8 p-4 shadow-lg h-full sm:h-1/2 sm:my-auto justify-center text-xl">
+
+			<div className="relative flex flex-col gap-2 py-4 w-full">
+			<Link 
+				className="absolute left-0 text-neutral-600 hover:text-neutral-700 hidden sm:flex"
+				to="/">
+				{`> Back`}
+			</Link>
+				 <h1 className="text-3xl font-bold">La comanda</h1>
+				 <h2 className="text-2xl text-neutral-700">Sign into your account</h2>
+			</div>
+
+			{/* Email input */}
+			 <div className="flex flex-col w-full max-w-sm">
+			 <input 
+				 value={email}
+				 onChange={e => setEmail(e.target.value)}
+				 aria-invalid={!!errors.email}
+				 aria-describedby={errors.email ? "email-error" : undefined}
+				 className="p-3 px-4 border rounded-xl placeholder-neutral-600 focus:border-red-200 aria-invalid:border-red-500"
+				 placeholder="email"/>
+				 {errors.email && (
+					 <div id="email-error">
+					 <ErrorMessage message={errors.email}/>
+					 </div>
+				 )}
 			 </div>
-			 {errors.email && (<ErrorMessage message={errors.email}/>)}
+
+			 {/* Password input */}
+			 <div className="flex flex-col w-full max-w-sm">
+				 <div className="relative flex items-center">
+				 <input 
+					 value={password}
+					 onChange={e => setPassword(e.target.value)}
+					 aria-invalid={!!errors.password}
+					 aria-describedby={errors.password ? "password-error" : undefined}
+					 className="p-3 px-4 border rounded-xl placeholder-neutral-600 focus:border-red-200 aria-invalid:border-red-500 w-full"
+					 placeholder="password"
+					 type={showPassword ? "text" : "password"}/>
+					 
+					 <button
+					 type="button"
+					 onClick={() => setShowPassword(!showPassword)}
+					 className="absolute right-3 text-neutral-500 hover:text-neutral-700"
+					 aria-label={showPassword ? "Hide password" : "Show password"}
+					 >
+					 {showPassword ? (
+						 <EyeIcon/>
+					 ) : (
+						 <EyeSlashIcon/>
+					 )}
+						</button>
+				 </div>
+				 {errors.password && (
+						 <div id="password-error">
+						 <ErrorMessage message={errors.password}/>
+						 </div>
+					 )}
 			 </div>
-			 <div>
-			 <div className="flex flex-col sm:flex-row gap-2">
-			 	<p className="sm:w-[90px] my-auto text-left text-neutral-600">
-			 	Password
-			 </p>
-				<input 
-					value={password}
-					onChange={e => setPassword(e.target.value)}
-					className="p-2 border border-neutral-200 rounded-xl"
-					placeholder="password"
-					type="password"/>
-			 </div>
-			 {errors.password && (<ErrorMessage message={errors.password}/>)}
-			 </div>
+
 			<button 
 				disabled={loading}
 				onClick={() => handleLogin()}
-				className={`${buttonVariants.base} w-1/3 mb-3 border border-neutral-600 rounded-lg`}>
-				{ loading ? 'Logging...' : 'Log' }
+				className={`${buttonVariants.primary} p-3 w-full max-w-sm mb-3 border border-neutral-600 rounded-lg`}>
+				{ loading ? 'Signing in...' : 'Sign in' }
 			</button>
 			</div>
 		</ScreenLayout>
