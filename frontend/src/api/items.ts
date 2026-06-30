8e5dcbd1747c0799d7d08b35da984a4f02be8ad4
Nahuel <nahuelfuentes2001@gmail.com>
2026-06-30 01:36:02 -0300

    feat: implement stock page

--- frontend/src/api/items.ts
+++ frontend/src/api/items.ts
@@ -3,6 +3,23 @@ import type { Item } from '../types'
 
 const API_URL = import.meta.env.VITE_API_URL
 
+export async function getStock(): Promise<Item []> {
+	const { data, error } = await supabase.auth.getSession()
+	const token = data.session?.access_token
+
+	if (!token) {
+		throw new Error('User is not authenticated')
+	}
+	const response = await fetch(`${API_URL}/items`, {
+		headers: {
+			'Authorization': `Bearer ${token}`
+		}
+	})
+
+	if (!response.ok) throw new Error('Items not found')
+	return response.json()
+}
+
 export async function getLowStockItems(): Promise<Item []> {
 	const { data, error } = await supabase.auth.getSession()
 	const token = data.session?.access_token
--- frontend/src/pages/Stock.tsx
+++ frontend/src/pages/Stock.tsx
@@ -1,9 +1,60 @@
-import ScreenLayout from "../layouts/ScreenLayout";
+import {useEffect, useState} from "react"
+import ScreenLayout from "../layouts/ScreenLayout"
+import type { Item } from "../types"
+import { getStock } from "../api/items"
+import EditableStockTable from "../components/EditableStockTable"
+import {buttonVariants} from "../components/ButtonStyles"
 
-export default function Stock() {
+export default function Stoc() {
+	const [allItems, setAllItems] = useState<Item[]>([])
+	const [searchQuery, setSearchQuery] = useState("")
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState<string | null>(null)
+
+	useEffect(() => {
+		async function loadItems() {
+			try {
+				setLoading(true)
+				const data = await getStock()
+				setAllItems(data)
+			} catch (err) {
+				setError(err)
+			} finally {
+				setLoading(false)
+			}
+		}
+		loadItems()
+	}, [])
+
+	const visibleItems = allItems.filter(item =>
+					     item.name.toLowerCase().includes(searchQuery.toLowerCase()))
+
+	if (loading) return (<div className="text-center p-12">Loading items...</div>)
+	if (error) return (<div className="text-center text-red-500">{error}</div>)
+
 	return (
 		<ScreenLayout>
-			Private stock screen
+			<div className="flex flex-col w-full gap-2 mt-2">
+
+			{/* Search and creation tab */}
+			<div className="flex flex-row sm:flex-row justify-between mx-2 gap-2">
+				{/* Search filter */}
+				<input
+					placeholder="Search by item name"
+					type="text"
+					value={searchQuery}
+					onChange={(e) => setSearchQuery(e.target.value)}
+					className="bg-neutral-300 px-3 w-full h-10 rounded-sm"/>	
+				<button
+					className={buttonVariants.secondary}>
+				+ Add item
+				</button>
+
+			</div>
+
+			{/* Items table */}
+			<EditableStockTable items={visibleItems}/>
+			</div>
 		</ScreenLayout>
 	)
 }
