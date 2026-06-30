c7db411ad50b10983aa23e22e6814ea055f63ac9
Nahuel <nahuelfuentes2001@gmail.com>
2026-06-30 01:32:07 -0300

    style: styling for card and button components

--- frontend/src/components/ButtonStyles.ts
+++ frontend/src/components/ButtonStyles.ts
@@ -1,5 +1,6 @@
 export const buttonVariants = {
 	base: "p-2 px-3 text-md rounded-sm hover:bg-neutral-700 hover:text-neutral-200 disabled:opacity-50 cursor-pointer ease-in duration-200 active:scale-90",
 	primary: "p-2 px-3 text-md rounded-xl bg-neutral-700 text-neutral-200 hover:bg-neutral-500 disabled:opacity-50 cursor-pointer ease-in duration-200 active:scale-90",
+	secondary: "p-2 px-3 text-md rounded-sm hover:bg-neutral-700 hover:text-neutral-200 disabled:opacity-50 cursor-pointer ease-in duration-200 active:scale-90 border border-dashed border-neutral-500 text-nowrap font-mono",
 	danger: "p-2 text-red-500 rounded-sm cursor-pointer hover:bg-red-500 hover:text-neutral-200 ease-in duration-200"
 }
---
+++ frontend/src/components/CardStyles.ts
@@ -0,0 +1,6 @@
+export const cardVariants = {
+	base: "flex flex-col bg-neutral-100 border border-neutral-300 rounded-lg justify-between p-3 overflown-hidden",
+	table: "flex flex-col bg-neutral-100 border border-neutral-300 text-center rounded-lg shadow-lg",
+	dashed: "flex flex-col bg-neutral-100 border border-dashed border-neutral-400 rounded-lg justify-between p-3",
+	danger: "flex flex-col bg-neutral-100 border border-red-400 rounded-lg justify-between p-2"
+}
