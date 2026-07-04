4b3e3878bc7b73931f5ffa465524a9e0a8c4d895
Nahuel <nahuelfuentes2001@gmail.com>
2026-07-04 02:48:07 -0300

    style: fix width of ModalLayout

--- frontend/src/layouts/ModalLayout.tsx
+++ frontend/src/layouts/ModalLayout.tsx
@@ -10,7 +10,7 @@ export default function ModalLayout( { onClose, children }: ModalLayoutProps ){
 	return (
 		<div 
 			onClick={onClose}
-			className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm p-4">
+			className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm p-4 w-full">
 			<div
 				onClick={(e) => e.stopPropagation()}
 				className={`${cardVariants.base} gap-4 p-4 w-full md:w-2/3 max-w-lg shadow-lg`}>
