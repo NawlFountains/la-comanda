023a83ad0b2532246c28cba6b67e920583b606d3
Nahuel <nahuelfuentes2001@gmail.com>
2026-07-07 22:57:47 -0300

    feat: add error screen on failed loading

---
+++ frontend/src/components/errors/ErrorLoading.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import ScreenLayout from '../../layouts/ScreenLayout'
+import errorImage from '../../assets/loading-cat.jpg'
+import { buttonVariants } from '../styles/ButtonStyles'
+
+
+interface ErrorLoadingProps {
+	message: string
+}
+
+export default function ErrorLoading({ message }: ErrorLoadingProps) {
+	console.error('Error: ',message)
+
+	return (
+	<ScreenLayout>
+		<div className='my-auto flex flex-col text-center items-center gap-5'>
+		<img
+			alt="cat-error"
+			src={errorImage}>
+			</img>
+		<p
+			className='text-red-500 text-xl'>
+			Failed to load data
+		</p>
+		<div className='grid grid-cols-1 md:grid-cols-2 content-evenly w-full md:w-2/3 gap-3'>
+			<button
+				className={`${buttonVariants.base}`}
+				onClick={() => window.location.reload()}>
+				Refresh
+			</button>
+			<Link 
+				className={`${buttonVariants.base} border`}
+				to="/">
+				Go to home screen
+			</Link>
+		</div>
+		</div>
+		</ScreenLayout>
+	)
+} 
