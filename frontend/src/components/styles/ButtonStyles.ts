33b8ec2db6d6e0bd88139a493a6c68d6a76eeaa6
Nahuel <nahuelfuentes2001@gmail.com>
2026-07-07 22:55:35 -0300

    styles: add style for toggle buttons

--- frontend/src/components/styles/ButtonStyles.ts
+++ frontend/src/components/styles/ButtonStyles.ts
@@ -1,6 +1,15 @@
+const SHARED_BASE = "p-2 px-3 text-md disabled:opacity-50 cursor-pointer ease-in duration-200 active:scale-90 h-10";
+
 export const buttonVariants = {
-	base: "p-2 px-3 text-md rounded-sm hover:bg-neutral-700 hover:text-neutral-200 disabled:opacity-50 cursor-pointer ease-in duration-200 active:scale-90",
-	primary: "p-2 px-3 text-md rounded-xl bg-neutral-700 text-neutral-200 hover:bg-neutral-500 disabled:opacity-50 cursor-pointer ease-in duration-200 active:scale-90",
-	secondary: "p-2 px-3 text-md rounded-sm hover:bg-neutral-700 hover:text-neutral-200 disabled:opacity-50 cursor-pointer ease-in duration-200 active:scale-90 border border-dashed border-neutral-500 text-nowrap font-mono",
+	base: `${SHARED_BASE} rounded-sm hover:bg-neutral-700 hover:text-neutral-200`,
+	
+	primary: `${SHARED_BASE} rounded-xl bg-neutral-700 text-neutral-200 hover:bg-neutral-500`,
+	
+	secondary: `${SHARED_BASE} rounded-sm hover:bg-neutral-700 hover:text-neutral-200 border border-dashed border-neutral-500 text-nowrap font-mono`,
+	
+	toggleOn: `${SHARED_BASE} rounded-lg bg-neutral-700 text-neutral-200 hover:bg-neutral-200 hover:text-neutral-700 font-mono border border-neutral-400`,
+	
+	toggleOff: `${SHARED_BASE} rounded-lg bg-neutral-200 text-neutral-700 hover:bg-neutral-700 hover:text-neutral-200 font-mono border border-neutral-400`,
+	
 	danger: "p-2 text-red-500 rounded-sm cursor-pointer hover:bg-red-500 hover:text-neutral-200 ease-in duration-200"
 }
