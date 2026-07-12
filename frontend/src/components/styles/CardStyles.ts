0aa0b86fae0e8e06a05a761b3c42ef1e53d2d666
Nahuel <nahuelfuentes2001@gmail.com>
2026-07-12 20:11:10 -0300

    style: add darkmode to cards and button styles

--- frontend/src/components/styles/CardStyles.ts
+++ frontend/src/components/styles/CardStyles.ts
@@ -1,6 +1,8 @@
+const SHARED_BASE = "flex flex-col bg-neutral-200 dark:bg-neutral-800 border rounded-lg"
+
 export const cardVariants = {
-	base: "flex flex-col bg-neutral-100 border border-neutral-300 rounded-lg justify-between p-3 overflown-hidden",
-	table: "flex flex-col bg-neutral-100 border border-neutral-300 text-center rounded-lg shadow-lg",
-	dashed: "flex flex-col bg-neutral-100 border border-dashed border-neutral-400 rounded-lg justify-between p-3",
-	danger: "flex flex-col bg-neutral-100 border border-red-400 rounded-lg justify-between p-2"
+	base: `${SHARED_BASE} border-neutral-300 dark:border-neutral-800 justify-between p-3 overflown-hidden`,
+	tableBody: `${SHARED_BASE} border-neutral-300 dark:border-neutral-600 text-center shadow-lg`,
+	dashed: `${SHARED_BASE} border-dashed border-neutral-400 dark:border-neutral-600 justify-between p-3`,
+	danger: `${SHARED_BASE} border border-red-400 justify-between p-2`
 }
